ec4cb9eadc0fcaf5be85e95dcf68b38beaddc73a
Chris S. Maume <chris.maume@linkanalytics.com>
2013-07-23 11:47:57 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Energy::Application.config.secret_token = '5444024957cf18b0d90f8a0e5627e2b5d1dc55991ed5ef1adef2ba1bc18a1f17320aa1a1ec6916f502908e29e883a86b9c4650de85e3368508de2b2927e95f14'
